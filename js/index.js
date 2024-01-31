e823d24710ec9e20c98aa20f47e99a8f71679d88
Marco Pala <marcopala1994@gmail.com>
2024-01-31 21:51:38 +0100

    modifica

--- js/index.js
+++ js/index.js
@@ -2,14 +2,16 @@ const inputbtn = document.getElementById("inputbtn");
 
 const newbtn = document.getElementById("btn");
 newbtn.onclick = function () {
-  const lista = document.getElementById("elenco");
-  const li = document.createElement("li");
-  li.innerText = inputbtn.value;
-  console.log(li);
-  lista.appendChild(li);
-};
+  if (inputbtn.value !== "") {
+    const lista = document.getElementById("elenco");
+    const li = document.createElement("li");
+    li.innerText = inputbtn.value;
+    console.log(li);
+    lista.appendChild(li);
+  }
 
-const pulisci = document.getElementById("clearbtn");
-pulisci.onclick = function () {
-  pulisci.removeEventListener("li");
+  const pulisci = document.getElementById("clearbtn");
+  pulisci.onclick = function () {
+    pulisci.remove("li");
+  };
 };
