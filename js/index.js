f15be07f6d7840a494ef218d5e592f264a4ecc3f
Marco Pala <marcopala1994@gmail.com>
2024-02-01 09:19:27 +0100

    sfvga

--- js/index.js
+++ js/index.js
@@ -1,17 +1,21 @@
-const inputbtn = document.getElementById("inputbtn");
+const newlist = [];
 
+const inputbtn = document.getElementById("inputbtn");
 const newbtn = document.getElementById("btn");
 newbtn.onclick = function () {
   if (inputbtn.value !== "") {
     const lista = document.getElementById("elenco");
     const li = document.createElement("li");
     li.innerText = inputbtn.value;
+    inputbtn.value = "";
+    const cancel = document.createElement("button");
+    //cancel.classList.add(btncancel)
+
+    cancel.onclick = function () {
+      li.remove();
+    };
+    li.appendChild(cancel);
     console.log(li);
     lista.appendChild(li);
-    const pulisci = document.getElementById("clearbtn");
-
-    pulisci.onclick = function () {
-      lista.remove();
-    };
   }
 };
